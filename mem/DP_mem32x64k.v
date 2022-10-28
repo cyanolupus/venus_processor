4c43a037047dd80ad1164c4ce564cda5a61ad801
cyanolupus <cyanolupus@gmail.com>
2022-10-28 12:45:30 +0900

    refactoring about memory

---
+++ mem/DP_mem32x64k.v
@@ -0,0 +1,31 @@
+module DP_mem32x64k(clk,
+                    A,
+                    W,
+                    D,
+                    Q);
+
+    `include "./include/params.v"
+
+    input clk;
+    input [ADDR -1: 0] A;
+    input W;
+    input [WORD -1: 0] D;
+    output [WORD -1: 0] Q;
+
+    reg [WORD -1: 0] mem_bank [0: LEN];
+
+    reg [WORD -1: 0] o_reg;
+
+    assign Q = o_reg;
+
+    always @(posedge clk) begin // or negedge rst)
+        if (W == 1'b1)
+            mem_bank[A] <= D; // write
+        else
+            o_reg <= mem_bank[A]; //read
+    end
+
+    initial begin
+        $readmemh("./mem/memfiles/mem.dat", mem_bank);
+    end
+endmodule
