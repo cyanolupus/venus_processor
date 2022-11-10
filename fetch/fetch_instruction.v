5cb17e2cf32a3fb9d4ba7d19caa80fd7262e08ff
cyanolupus <cyanolupus@gmail.com>
2022-11-10 23:56:01 +0900

    fix: rethink about stall and branch to catch up with decode_instruction

--- fetch/fetch_instruction.v
+++ fetch/fetch_instruction.v
@@ -22,40 +22,35 @@ module fetch_instruction(clk, reset,
     input [ADDR -1: 0] branch_addr_i;
 
     reg v_r;
-    reg [WORD -1: 0] inst_r;
     reg [ADDR -1: 0] mem_r;
     reg [ADDR -1: 0] pc_r;
-    reg stall_prev;
+    reg [WORD -1: 0] inst_r;
 
-    wire [ADDR -1: 0] pc_next;
+    wire [ADDR -1: 0] mem_next;
+    wire [ADDR -1: 0] mem_prev;
     wire [ADDR -1: 0] pc_prev;
 
-    assign inst_o = (stall_i)? inst_r: inst_i;
-    assign mem_o = (stall_i)? pc_prev: mem_r;
-    assign pc_next = mem_r + 1;
-    assign pc_prev = mem_r - 1;
+    assign inst_o = stall_i?inst_r:inst_i;
+    assign mem_next = branch_i?(branch_addr_i + 1):(mem_r + 1);
+    assign mem_prev = mem_r - 1;
+    assign pc_prev = pc_r - 1;
+    assign mem_o = stall_i?mem_prev:(branch_i?branch_addr_i:mem_r);
     assign v_o = v_r;
-    assign stall_o = stall_i;
-    assign pc_o = pc_r;
+    assign stall_o = stall_i & v_r;
+    assign pc_o = stall_i?pc_prev:pc_r;
 
     always @(posedge clk or negedge reset) begin
         if (~reset) begin
             mem_r <= 0;
             v_r <= 0;
-            inst_r <= 0;
-            stall_prev <= 0;
             pc_r <= 0;
+            inst_r <= 0;
         end else begin
             if (~stall_i) begin
+                pc_r <= mem_o;
                 inst_r <= inst_i;
-                pc_r <= mem_r;
-                if (branch_i) begin
-                    mem_r <= branch_addr_i;
-                    v_r <= 0;
-                end else begin
-                    mem_r <= pc_next;
-                    v_r <= 1;
-                end
+                mem_r <= mem_next;
+                v_r <= 1;
             end
         end
     end
