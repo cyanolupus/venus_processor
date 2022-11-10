999281c8fcf8a7a3bddca0aaa8269345cf25956e
cyanolupus <cyanolupus@gmail.com>
2022-11-10 23:54:00 +0900

    fix typo

--- exec/exec_branch.v
+++ exec/exec_branch.v
@@ -1,5 +1,5 @@
 module exec_branch (opr0_i, opr1_i,
-                v_i, pc_i, abs_i,
+                v_i, pc_i, opecode_i,
                 carry_flag_i, zero_flag_i,
                 sign_flag_i, overflow_flag_i,
                 branch_o, branch_addr_o);
@@ -10,11 +10,13 @@ module exec_branch (opr0_i, opr1_i,
     input [W_OPR -1: 0] opr0_i, opr1_i;
     input v_i;
     input [ADDR -1: 0] pc_i;
-    input abs_i;
+    input [W_OPC -1: 0] opecode_i;
     input carry_flag_i, zero_flag_i, sign_flag_i, overflow_flag_i;
     output branch_o;
     output [ADDR -1: 0] branch_addr_o;
 
-    assign branch_o = v_i & select3from8(opr0_i[2:0], 1'b1, zero_flag_i, (~sign_flag_i), sign_flag_i, carry_flag_i, overflow_flag_i, 1'b0, 1'b0);
-    assign branch_addr_o = (abs_i)? opr1_i : (pc_i + opr1_i);
+    wire abs = opecode_i[0];
+
+    assign branch_o = (opecode_i[W_OPC -1:1] == 6'b001_110)? v_i & select3from8(opr0_i[2:0], 1'b1, zero_flag_i, (~sign_flag_i), sign_flag_i, carry_flag_i, overflow_flag_i, 1'b0, 1'b0) : 1'b0;
+    assign branch_addr_o = (abs)? opr1_i : (pc_i + opr1_i);
 endmodule
