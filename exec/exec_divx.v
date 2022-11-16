bbb03713e0de9bfb18447d78a60fac7e95a17f4f
cyanolupus <cyanolupus@gmail.com>
2022-11-16 15:18:41 +0900

    add flags output to divx shift

--- exec/exec_divx.v
+++ exec/exec_divx.v
@@ -1,7 +1,19 @@
-module exec_divx (opr0_i, opr1_i, result_o);
+module exec_divx (opr0_i, opr1_i, result_o, flags_o);
     `include "./include/params.v"
     input [W_OPR -1: 0] opr0_i, opr1_i;
     output [W_OPR -1: 0] result_o;
+    output [W_FLAGS -1: 0] flags_o;
+
+    wire carry;
+    wire zero;
+    wire sign;
+    wire overflow;
 
     assign result_o = opr0_i / opr1_i;
+
+    assign carry = 0;
+    assign zero = ~(|result_o);
+    assign sign = opr0_i[W_OPR -1] ^ opr1_i[W_OPR -1];
+    assign overflow = sign ^ result_o[W_OPR -1];
+    assign flags_o = {overflow, sign, zero, carry};
 endmodule
--- exec/exec_shift.v
+++ exec/exec_shift.v
@@ -1,17 +1,33 @@
-module exec_shift (opr0_i, opr1_i, result_o, select_i);
+module exec_shift (opr0_i, opr1_i, result_o, select_i, flags_o);
     `include "./include/params.v"
     `include "./include/select2from4.v"
     input signed [W_OPR -1: 0] opr0_i, opr1_i;
     output [W_OPR -1: 0] result_o;
     input [1:0] select_i;
+    output [W_FLAGS -1: 0] flags_o;
+
+    wire carry;
+    wire zero;
+    wire sign;
+    wire overflow;
 
     wire [4:0] opr1_pre;
-    wire [W_OPR -1: 0] result_shl, result_shr, result_ash;
+    wire signed [W_OPR*2 -1: 0] opr0_pre_r, opr0_pre_l;
+    wire [W_OPR*2 -1: 0] result_shl, result_shr, result_ash;
+    wire [W_OPR -1: 0] result_over;
 
     assign opr1_pre = opr1_i[4:0];
-    assign result_shl = opr0_i << opr1_i;
-    assign result_shr = opr0_i >> opr1_i;
-    assign result_ash = opr0_i >>> opr1_i;
+    assign opr0_pre_r = {opr0_i, W_OPR{1'b0}};
+    assign opr0_pre_l = {W_OPR{1'b0}, opr0_i};
+    assign result_shl = opr0_pre_l << opr1_pre;
+    assign result_shr = opr0_pre_r >> opr1_pre;
+    assign result_ash = opr0_pre_r >>> opr1_pre;
 
-    assign result_o = select2from4(select_i, result_shl, result_shr, result_ash, result_ash);
+    assign result_o = select2from4(select_i, result_shl[W_OPR -1:0], result_shr[W_OPR*2 -1:W_OPR], result_ash[W_OPR*2 -1:W_OPR], 32'b0);
+    assign result_over = select2from4(select_i, result_shl[W_OPR*2 -1:W_OPR], result_shr[W_OPR -1:0], result_ash[W_OPR -1:0], 32'b0);
+    assign carry = |result_over;
+    assign zero = ~(|result_o);
+    assign sign = result_o[W_OPR -1];
+    assign overflow = opr0_i[W_OPR -1] ^ sign;
+    assign flags_o = {overflow, sign, zero, carry};
 endmodule
