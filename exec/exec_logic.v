a629deda75f6fa4d60d4f986954abf6cf352498f
cyanolupus <cyanolupus@gmail.com>
2022-11-16 13:29:01 +0900

    add logic unit

--- exec/exec_logic.v
+++ exec/exec_logic.v
@@ -11,5 +11,10 @@ module exec_logic (opr0_i, opr1_i, result_o, select_i);
     wire [W_OPR -1: 0] xor_pre;
     wire [W_OPR -1: 0] not_pre;
 
+    assign and_pre = opr0_i & opr1_i;
+    assign or_pre = opr0_i | opr1_i;
+    assign xor_pre = opr0_i ^ opr1_i;
+    assign not_pre = ~opr0_i;
+
     assign result_o = select2from4(select_i, and_pre, or_pre, not_pre, xor_pre);
 endmodule
