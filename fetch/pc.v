b99273226b34a76c2c440963d1f239f451dc3427
cyanolupus <cyanolupus@gmail.com>
2022-12-15 03:04:04 +0900

    add pred handler

--- fetch/pc.v
+++ fetch/pc.v
@@ -1,7 +1,8 @@
 module pc(clk, reset,
             stall_i, stall_o,
             pc_o,
-            branch_i, branch_addr_i);
+            branch_i, branch_addr_i,
+            pred_i, pred_addr_i);
 
     `include "../include/params.v"
 
@@ -12,6 +13,8 @@ module pc(clk, reset,
 
     input branch_i;
     input [ADDR -1: 0] branch_addr_i;
+    input pred_i;
+    input [ADDR -1: 0] pred_addr_i;
 
     reg [ADDR -1: 0] pc_r;
     reg [ADDR -1: 0] pc_prev;
@@ -28,6 +31,8 @@ module pc(clk, reset,
         end else begin
             if (branch_i) begin
                 pc_r <= branch_addr_i;
+            end else if (pred_i) begin
+                pc_r <= pred_addr_i + 1;
             end else if (~stall_i) begin
                 pc_prev <= pc_r;
                 pc_r <= pc_next;
