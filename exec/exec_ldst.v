45454e6cb351607af350fa09dfce0c0760e8e3f0
cyanolupus <cyanolupus@gmail.com>
2022-12-21 14:29:30 +0900

    fix bit width

--- exec/exec_ldst.v
+++ exec/exec_ldst.v
@@ -11,8 +11,14 @@ module exec_ldst (opr0_i, opr1_i, immf_i, imm_i, stf_i, addr_o, write_o, data_o)
     wire [ADDR -1: 0] load_addr;
     wire [ADDR -1: 0] store_addr;
 
-    assign load_addr = immf_i?(opr1_i + imm_i):opr1_i;
-    assign store_addr = immf_i?(opr0_i + imm_i):opr0_i;
+    wire [ADDR -1: 0] opr0_addr;
+    wire [ADDR -1: 0] opr1_addr;
+
+    assign opr0_addr = opr0_i[ADDR -1: 0];
+    assign opr1_addr = opr1_i[ADDR -1: 0];
+
+    assign load_addr = immf_i?(opr1_addr + imm_i):opr1_addr;
+    assign store_addr = immf_i?(opr0_addr + imm_i):opr0_addr;
     assign addr_o = (stf_i) ? store_addr[ADDR -1: 0] : load_addr[ADDR -1: 0];
     assign data_o = opr1_i;
     assign write_o = stf_i;
